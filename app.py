52b8a1790eb68b987dbefa19e0ac490c0b2ad092
Mattia <mattiatiburzi01@gmail.com>
2023-08-02 01:38:15 +0200

    ciao mondo

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/ita")
+def ciao_mondo():
+    return "<p>Ciao Mondo!</p>"
+
 if __name__ == '__main__':
   app.run(debug=False, host='0.0.0.0')
